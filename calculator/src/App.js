24ecaa29ee40541efa544aaa8913dfda047070da
Zachery Converse <zacheryconverse@gmail.com>
2020-12-16 11:31:22 -0700

    convert to cra

---
+++ calculator/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import Button from './components/Button.js';
+import Input from './components/Input';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='calc-wrapper'>
+        <div className='row'>
+          <Input />
+        </div>
+        <div className='row'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>÷</Button>
+        </div>
+        <div className='row'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>x</Button>
+        </div>
+        <div className='row'>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className='row'>
+          <Button>.</Button>
+          <Button>0</Button>
+          <Button>=</Button>
+          <Button>-</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator/src/components/Input.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Input.css';
+
+class Input extends Component {
+  render() {
+    return (
+      <div className='input'>
+        hello
+      </div>
+    )
+  }
+};
+
+export default Input;
